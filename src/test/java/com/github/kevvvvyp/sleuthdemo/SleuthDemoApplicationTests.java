1c1b462f72d0bb0b30225d72b8161dc30eda785c
kevin.paton <kevin.paton@cgi.com>
2020-10-30 15:59:27 +0000

    Initial commit

---
+++ src/test/java/com/github/kevvvvyp/sleuthdemo/SleuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kevvvvyp.sleuthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
